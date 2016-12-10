f3f2d585a271b50846f3de5dc107b2a7e36188c1
363905547@qq.com <363905547@qq.com>
2016-12-10 13:46:27 +0800

    Test
    tet

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get('/', function (req, res, next) {
     });
 });
 
-app.listen(3000, function () {
-  console.log('app is listening at port 3000');
+app.listen(process.env.PORT || 5000, function () {
+  console.log('app is listening at port 5000');
 });
